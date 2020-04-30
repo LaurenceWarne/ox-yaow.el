cec4fa0d9d1fa72798848928ece736d1f7569606
lWarne <laurencewarne@gmail.com>
2020-04-30 14:31:27 +0100

    Initial commit. Add License, Eldev files.

---
+++ yaow.el
@@ -0,0 +1,35 @@
+;;; yaow.el --- Generate html pages from org files -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020 Laurence Warne
+
+;; Author: Laurence Warne
+;; Maintainer: Laurence Warne
+;; Version: 0.1
+;; Keywords: org
+;; URL: https://github.com/LaurenceWarne/yaow.el
+;; Package-Requires: ((emacs "26"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; This package adds another org html export option which aims to provide a
+;; lightweight wiki.
+
+;;; Code:
+
+
+(provide 'yaow)
+
+;;; yaow.el ends here
