b15c7d9f6c2eb684ed3551dff56c45f4966c296f
lWarne <laurencewarne@gmail.com>
2020-05-08 12:37:35 +0100

    Add more tests for yaow--get-adjacent-files.

--- tests/test-yaow.el
+++ tests/test-yaow.el
@@ -15,16 +15,30 @@
 		    :var ((files-in-dir '("a.org" "b.org" "hi.org" "z.org")))
 
 		    (it "should return correct adjacent files for middle file"
-		     (expect (yaow--get-adjacent-files "b.org" files-in-dir)
-			     :to-equal
-			     '("a.org" . "hi.org")))
+			(expect (yaow--get-adjacent-files "b.org" files-in-dir)
+				:to-equal
+				'("a.org" . "hi.org")))
 
 		    (it "should return sole adjacent file for beginning file"
-		     (expect (yaow--get-adjacent-files "a.org" files-in-dir)
-			     :to-equal
-			     '(nil . "b.org")))
+			(expect (yaow--get-adjacent-files "a.org" files-in-dir)
+				:to-equal
+				'(nil . "b.org")))
 
 		    (it "should return sole adjacent file for end file"
-		     (expect (yaow--get-adjacent-files "z.org" files-in-dir)
-			     :to-equal
-			     '("hi.org" . nil)))))
+			(expect (yaow--get-adjacent-files "z.org" files-in-dir)
+				:to-equal
+				'("hi.org" . nil)))
+
+		    (it "should return no adjacent files for one file in list"
+			(expect (yaow--get-adjacent-files "z.org" '("z.org"))
+				:to-equal
+				'(nil . nil)))
+
+		    (it "should return correct adjacent files for no sort"
+			(let ((files-in-dir-no-sort
+			       '("z.org" "b.org" "hi.org" "a.org")))
+			  (expect (yaow--get-adjacent-files "b.org"
+							    files-in-dir-no-sort
+							    :sort nil)
+				  :to-equal
+				  '("z.org" . "hi.org"))))))
